318b92b20e51f1b5632044091e5c8352bb55c64a
grafuj <sfrrki@gmail.com>
2026-09-19 21:12:32 -0700

    change it to Dvd logo and use an image. it adapts to image size and borders (if added!)

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import DvdLogo from './mainPage.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <DvdLogo />
   </StrictMode>,
 )
